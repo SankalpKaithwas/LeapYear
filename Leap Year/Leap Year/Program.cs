331879861092877bf2d577eaebecf556bb4c52e8
unknown <kaithwas.697@gmail.com>
2022-03-05 12:43:51 +0530

    Program to check Leap year

---
+++ Leap Year/Leap Year/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+Console.WriteLine("Enter a year:");
+
+int year = int.Parse(Console.ReadLine());
+if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+{
+    Console.WriteLine(year + "is a leap Year ");
+}
+else
+{
+    Console.WriteLine("{0} is not a leap Year ", year);
+}
+
+
